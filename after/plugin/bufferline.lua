94cc331317e1c82c55c5573c270b7827b12121ef
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-07-09 10:38:57 -0400

    refactor: set the show_buffer_close_icons attribute for the bufferline plugin to true

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -16,7 +16,7 @@ require("bufferline").setup({
 			},
 		},
 		show_buffer_icons = true,
-		show_buffer_close_icons = false,
+		show_buffer_close_icons = true,
 		show_close_icon = true,
 		show_tab_indicator = true,
 		separator_style = "line",
