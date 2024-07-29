5611212767d30fb82cf70acb2dbae3b6ed5af4da
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-07-28 21:25:38 -0400

    refactor: add the fzf package to the brew install command

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the base tools
-brew install neovim ripgrep git lazygit shellcheck stylua zsh-autosuggestions zsh-syntax-highlighting eza
+brew install neovim fzf ripgrep git lazygit shellcheck stylua zsh-autosuggestions zsh-syntax-highlighting eza
 
 # Configure zsh-autosuggestions
 echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
