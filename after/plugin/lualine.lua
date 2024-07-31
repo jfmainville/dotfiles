3442f2a58e62d098db43ea3e992d01ffaae406f3
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-07-30 20:50:27 -0400

    chore: format the lualine.lua file

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -17,8 +17,8 @@ require("lualine").setup({
 	icons_enabled = true,
 	options = {
 		theme = custom_auto,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 	},
 	disabled_filetypes = {
 		statusline = {},
