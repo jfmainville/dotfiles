12339269c2167c11e7066419fb6765982ff1f4d7
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-08-06 13:51:58 -0400

    refactor: add additional keymaps for the vim-fugitive plugin

--- lua/jfmainville/keymap.lua
+++ lua/jfmainville/keymap.lua
@@ -42,6 +42,9 @@ end)
 vim.keymap.set("n", "<leader>gg", "<cmd>tab Git<CR>")
 vim.keymap.set("n", "<leader>gl", "<cmd>vertical Git log -p -- %<CR>")
 vim.keymap.set("n", "<leader>gd", "<cmd>Gdiff<CR>")
+vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
+vim.keymap.set("n", "<leader>gh", ':Git commit -m "')
+vim.keymap.set("n", "<leader>ga", ':Git commit -am "')
 -- Start the DAP debugger
 vim.keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.continue()<CR>")
 -- Add a breakpoint DAP debugger
