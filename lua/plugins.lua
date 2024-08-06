fa989debb032f8ae34df91c8cd9b4fef35291ddb
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-08-06 19:51:05 -0400

    refactor: update the Packer plugin base configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,15 @@
-vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
@@ -33,7 +44,7 @@ return require("packer").startup(function(use)
 			{ "tpope/vim-repeat" },
 		},
 	})
-  use("lewis6991/gitsigns.nvim")
+	use("lewis6991/gitsigns.nvim")
 	use("tpope/vim-fugitive")
 	use({ "nvim-lualine/lualine.nvim", requires = { "nvim-tree/nvim-web-devicons", opt = true } })
 	use("stevearc/oil.nvim")
@@ -70,4 +81,7 @@ return require("packer").startup(function(use)
 			{ "rafamadriz/friendly-snippets" },
 		},
 	})
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
