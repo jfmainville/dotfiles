0996976b9d8a2ad97d696abd0a9142a791108a42
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-08-08 14:12:53 -0400

    refactor: update the automatic update checker configuration for the lazy.nvim plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true, notify = true },
+	checker = { enabled = true, notify = false },
 	-- Disable the change detection notification
 	change_detection = { enabled = false },
 })
