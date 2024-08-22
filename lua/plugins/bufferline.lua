6d95fb1b78a578c023e3d462b74764fbd41965a0
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-08-21 20:27:08 -0400

    refactor: remove the diagnostics information from the bufferline plugin

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -102,11 +102,6 @@ return {
 				style = "none",
 			},
 			separator_style = "line",
-			diagnostics = "nvim_lsp",
-			diagnostics_indicator = function(count, level, diagnostics_dict, context)
-				local icon = level:match("error") and " " or " "
-				return " " .. icon .. count
-			end,
 		},
 		highlights = {
 			fill = {
