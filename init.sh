58f83eb705dd7094b174ddfe8d5de265f801c6ae
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-08-26 19:46:52 -0400

    refactor: remove the Lazygit and TMUX configurations

--- init.sh
+++ init.sh
@@ -12,12 +12,6 @@ echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlight
 # Configure EZA
 echo 'alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"' >>~/.zshrc
 
-# Copy the custom LazyGit configuration over to the application folder
-cp -f ~/.config/nvim/lazygit/config.yml ~/Library/Application\ Support/lazygit/
-
-# Copy the custom tmux configuration file to the application folder
-cp -f ~/.config/nvim/tmux/tmux.conf ~/.config/tmux/
-
 # LSPs and Formatters packages
 pip3 install black ansible-lint
 sudo npm install -g typescript-language-server pyright @ansible/ansible-language-server dockerfile-language-server-nodejs prettier bash-language-server intelephense @prisma/language-server vscode-langservers-extracted @tailwindcss/language-server
