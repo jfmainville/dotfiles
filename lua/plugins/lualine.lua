fd5339b189bf159a544156089bf1df68554116a9
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-09-08 22:50:40 -0400

    feat: add the macro recording status to the lualine plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,14 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local function show_macro_recording()
+			local recording_register = vim.fn.reg_recording()
+			if recording_register == "" then
+				return ""
+			else
+				return "Recording @" .. recording_register
+			end
+		end
 		local custom_auto = require("lualine.themes.auto")
 
 		custom_auto.normal.c.fg = "#100128"
@@ -39,7 +47,14 @@ return {
 			extensions = { "oil", "lazy", "mason" },
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff" },
+				lualine_b = {
+					"branch",
+					"diff",
+					{
+						"macro-recording",
+						fmt = show_macro_recording,
+					},
+				},
 				lualine_c = {
 					{
 						"filename",
