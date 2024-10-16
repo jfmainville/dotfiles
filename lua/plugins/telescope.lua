a3122920b03b80d856428935f375029fa9b1e5e0
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-10-15 20:47:57 -0400

    refactor: add the BUFFER_SEARCHER function to manage buffers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,48 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	config = function()
 		local builtin = require("telescope.builtin")
+		local action_state = require("telescope.actions.state")
+		local actions = require("telescope.actions")
+
+		BUFFER_SEARCHER = function()
+			builtin.buffers({
+				sort_mru = true,
+				ignore_current_buffer = true,
+				show_all_buffers = false,
+				attach_mappings = function(prompt_bufnr, map)
+					local refresh_buffer_searcher = function()
+						actions.close(prompt_bufnr)
+						vim.schedule(BUFFER_SEARCHER)
+					end
+
+					local delete_buf = function()
+						local selection = action_state.get_selected_entry()
+						vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+						refresh_buffer_searcher()
+					end
+
+					local delete_multiple_buf = function()
+						local picker = action_state.get_current_picker(prompt_bufnr)
+						local selection = picker:get_multi_selection()
+						for _, entry in ipairs(selection) do
+							vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+						end
+						refresh_buffer_searcher()
+					end
+
+					map("n", "dd", delete_buf)
+					map("n", "<C-d>", delete_multiple_buf)
+					map("i", "<C-d>", delete_multiple_buf)
+
+					return true
+				end,
+			})
+		end
+
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>gf", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fg", builtin.git_status, {})
+		vim.keymap.set("n", "<leader>fb", BUFFER_SEARCHER, {})
 		vim.keymap.set("n", "<leader>fe", function()
 			builtin.find_files({ search_dirs = { "~/.config/nvim", "~/software", "~/Projects" } })
 		end, {})
