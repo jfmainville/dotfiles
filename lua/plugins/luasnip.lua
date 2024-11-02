73ebc3c356a287ad28c94db72f8678116517f5ab
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-11-02 15:30:09 -0400

    refactor: add the missing dependency to the LuaSnip plugin

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,5 +1,6 @@
 return {
 	"L3MON4D3/LuaSnip",
+	dependencies = { "rafamadriz/friendly-snippets" },
 	config = function()
 		require("luasnip").filetype_extend("typescript", { "javascript" })
 		require("luasnip.loaders.from_vscode").lazy_load()
