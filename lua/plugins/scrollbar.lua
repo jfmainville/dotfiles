3127064856403082fcad9ed01677029111c78a04
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-11-02 23:20:17 -0400

    refactor: add the handle array to the scrollbar plugin

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -3,6 +3,14 @@ return {
 	dependencies = { "lewis6991/gitsigns.nvim" },
 	config = function()
 		require("scrollbar").setup({
+			handle = {
+				text = " ",
+				blend = 0, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
+				color = nil,
+				color_nr = nil, -- cterm
+				highlight = "CursorColumn",
+				hide_if_all_visible = true, -- Hides handle if all lines are visible
+			},
 			handlers = {
 				cursor = true,
 				diagnostic = true,
