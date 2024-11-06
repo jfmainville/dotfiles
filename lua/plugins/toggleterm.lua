9b7b48b8d6fd0b5f5b63c6a73c7020ed72cd9c94
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-11-05 21:59:16 -0500

    feat: add the toggleterm plugin to the project

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,29 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			size = 20,
+			open_mapping = [[<C-j>]],
+			shade_terminals = false,
+			persist_mode = false,
+			start_in_insert = true,
+			direction = "horizontal",
+
+			vim.keymap.set("n", "<leader>ta", function()
+				vim.cmd("silent! ToggleTermToggleAll")
+			end),
+		})
+
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "jj", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+			vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+		end
+
+		-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+		vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+	end,
+}
