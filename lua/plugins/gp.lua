28feefc5033b477c917aa8e6aac9c634e2b79eec
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-11-06 22:47:56 -0500

    refactor: update the toggle_target configuration to split in the gp.nvim plugin

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -48,7 +48,7 @@ return {
 			chat_free_cursor = true,
 			chat_confirm_delete = false,
 			chat_finder_pattern = "topic ",
-			toggle_target = "vsplit",
+			toggle_target = "split",
 			chat_shortcut_respond = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>g" },
 			chat_shortcut_delete = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>d" },
 			chat_shortcut_stop = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>s" },
