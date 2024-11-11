f63396dfc73b5509e6294740fa729dc7d1dc2628
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-11-10 20:39:36 -0500

    refactor: add the vertical window navigation keymap in the toggleterm plugin

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -25,6 +25,7 @@ return {
 			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
 			vim.keymap.set("t", "jj", [[<C-\><C-n>]], opts)
 			vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+			vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
 			vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 		end
 
