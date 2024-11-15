75f301de3148de53db415e033e8d50145df81890
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-11-14 21:44:46 -0500

    refactor: update the chat_shortcut_respond variable configuration in the gp.nvim plugin

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -69,7 +69,7 @@ return {
 			chat_confirm_delete = false,
 			chat_finder_pattern = "topic ",
 			toggle_target = "split",
-			chat_shortcut_respond = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>g" },
+			chat_shortcut_respond = { modes = { "n" }, shortcut = "<CR>" },
 			chat_shortcut_delete = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>d" },
 			chat_shortcut_stop = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>s" },
 			chat_shortcut_new = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>c" },
