f3cdaa2d98fdcb9e17fda6446e3fce34e2b997e9
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-11-27 22:22:34 -0500

    feat: add the neoscroll plugin to the project

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,15 @@
+return {
+	"karb94/neoscroll.nvim",
+	config = function()
+		require("neoscroll").setup({
+			mappings = { -- Keys to be mapped to their corresponding default scrolling animation
+				"<C-u>",
+				"<C-d>",
+				"<C-b>",
+				"<C-f>",
+				"<C-y>",
+				"<C-e>",
+			},
+		})
+	end,
+}
