42f5c48447d605cee0889f9ea2155898b8d038c8
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-01-27 21:54:43 -0500

    refactor: add a keymap to toggle the markdown preview mode in the markview plugin

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,6 +1,14 @@
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
+	keys = {
+		{
+			mode = "n",
+			"<leader>mt",
+			"<cmd>Markview Toggle<CR>",
+			desc = "Toggle the Markview markdown preview mode",
+		},
+	},
 	config = function()
 		local presets = require("markview.presets")
 
