a80e486ead65eadcce2f104202668561d25b36c3
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-02-11 20:54:53 -0500

    refactor: add the base configuration for the Wezterm terminal

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,29 +4,38 @@ local config = wezterm.config_builder()
 -- Launch the WSL:Ubuntu command line tool if on Windows
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_domain = "WSL:Ubuntu"
+	config.font_size = 12.0
+else
+	config.default_domain = "Default"
+	config.font_size = 14.0
 end
 
 -- Customizations
 config.font = wezterm.font("JetBrains Mono")
+config.cursor_blink_rate = 400
+config.visual_bell = {
+	fade_in_function = "Linear",
+	fade_in_duration_ms = 0,
+	fade_out_function = "Linear",
+	fade_out_duration_ms = 0,
+}
 config.colors = {
 	-- The default text color
-	foreground = "silver",
+	foreground = "#efeffd",
 	-- The default background color
-	background = "black",
+	background = "#1e0c32",
 
 	-- Overrides the cell background color when the current cell is occupied by the
 	-- cursor and the cursor style is set to Block
-	cursor_bg = "#52ad70",
-	-- Overrides the text color when the current cell is occupied by the cursor
-	cursor_fg = "black",
+	cursor_bg = "#c0ff98",
 	-- Specifies the border color of the cursor when the cursor style is set to Block,
 	-- or the color of the vertical or horizontal bar when the cursor style is set to
 	-- Bar or Underline.
-	cursor_border = "#52ad70",
+	cursor_border = "#c0ff98",
 	-- the foreground color of selected text
-	selection_fg = "black",
+	selection_fg = "#efeffd",
 	-- the background color of selected text
-	selection_bg = "#fffacd",
+	selection_bg = "#492949",
 
 	-- The color of the scrollbar "thumb"; the portion that represents the current viewport
 	scrollbar_thumb = "#222222",
@@ -34,74 +43,23 @@ config.colors = {
 	-- The color of the split lines between panes
 	split = "#444444",
 
-	ansi = {
-		"black",
-		"maroon",
-		"green",
-		"olive",
-		"navy",
-		"purple",
-		"teal",
-		"silver",
-	},
-	brights = {
-		"grey",
-		"red",
-		"lime",
-		"yellow",
-		"blue",
-		"fuchsia",
-		"aqua",
-		"white",
-	},
-
-	-- Arbitrary colors of the palette in the range from 16 to 255
-	indexed = { [136] = "#af8700" },
-
-	-- Since: 20220319-142410-0fcdea07
-	-- When the IME, a dead key or a leader key are being processed and are effectively
-	-- holding input pending the result of input composition, change the cursor
-	-- to this color to give a visual cue about the compose state.
-	compose_cursor = "orange",
-
-	-- Colors for copy_mode and quick_select
-	-- available since: 20220807-113146-c2fee766
-	-- In copy_mode, the color of the active text is:
-	-- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
-	-- 2. selection_* otherwise
-	copy_mode_active_highlight_bg = { Color = "#000000" },
-	-- use `AnsiColor` to specify one of the ansi color palette values
-	-- (index 0-15) using one of the names "Black", "Maroon", "Green",
-	--  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
-	-- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
-	copy_mode_active_highlight_fg = { AnsiColor = "Black" },
-	copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
-	copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
-
-	quick_select_label_bg = { Color = "peru" },
-	quick_select_label_fg = { Color = "#ffffff" },
-	quick_select_match_bg = { AnsiColor = "Navy" },
-	quick_select_match_fg = { Color = "#ffffff" },
-}
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
-config.colors = {
+	-- Tab Customizations
 	tab_bar = {
 		-- The color of the strip that goes along the top of the window
 		-- (does not apply when fancy tab bar is in use)
-		background = "#0b0022",
+		background = "#1e0c32",
 
 		-- The active tab is the one that has focus in the window
 		active_tab = {
 			-- The color of the background area for the tab
-			bg_color = "#2b2042",
+			bg_color = "#fc199a",
 			-- The color of the text for the tab
-			fg_color = "#c0c0c0",
+			fg_color = "#f8f8f2",
 
 			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
 			-- label shown for this tab.
 			-- The default is "Normal"
-			intensity = "Normal",
+			intensity = "Bold",
 
 			-- Specify whether you want "None", "Single" or "Double" underline for
 			-- label shown for this tab.
@@ -119,8 +77,8 @@ config.colors = {
 
 		-- Inactive tabs are the tabs that do not have focus
 		inactive_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
+			bg_color = "#1e0c32",
+			fg_color = "#f8f8f2",
 
 			-- The same options that were listed under the `active_tab` section above
 			-- can also be used for `inactive_tab`.
@@ -131,7 +89,7 @@ config.colors = {
 		inactive_tab_hover = {
 			bg_color = "#3b3052",
 			fg_color = "#909090",
-			italic = true,
+			italic = false,
 
 			-- The same options that were listed under the `active_tab` section above
 			-- can also be used for `inactive_tab_hover`.
@@ -158,6 +116,38 @@ config.colors = {
 		},
 	},
 }
-config.enable_scroll_bar = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.tab_bar_at_bottom = false
+config.use_fancy_tab_bar = false
+config.enable_scroll_bar = false
+
+-- Keymaps
+local act = wezterm.action
+config.keys = {
+	{
+		key = "I",
+		mods = "SUPER|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{ key = ",", mods = "SUPER|SHIFT", action = act.MoveTabRelative(-1) },
+	{ key = ".", mods = "SUPER|SHIFT", action = act.MoveTabRelative(1) },
+	{
+		key = "X",
+		mods = "SUPER|SHIFT",
+		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+}
 
 return config
