83cf7fffe5ea075073132d101c1275f91f71f82d
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-03-08 17:13:25 -0500

    refactor: add additional keymaps to the neogit plugin

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -11,6 +11,35 @@ return {
 			"<cmd>Neogit<CR>",
 			desc = "Open the Neogit interface",
 		},
+		{
+			mode = "n",
+			"<leader>gh",
+			function()
+				vim.cmd("Gitsigns stage_hunk")
+				vim.cmd("Gitsigns preview_hunk_inline")
+				vim.fn.feedkeys(':!git commit -m "')
+			end,
+			"",
+			desc = "Quick commit the current hunk using vim-fugitive",
+		},
+		{
+			mode = "n",
+			"<leader>ga",
+			function()
+				vim.cmd("!git add %")
+				vim.fn.feedkeys(':!git commit -m "')
+			end,
+			desc = "Quick commit all the hunks in the current file using vim-fugitive",
+		},
+		{
+			mode = "n",
+			"<leader>ga",
+			function()
+				vim.cmd("!git add %")
+				vim.fn.feedkeys(':!git commit -m "')
+			end,
+			desc = "Quick commit all the hunks in the current file using vim-fugitive",
+		},
 	},
 	config = function()
 		require("neogit").setup({
