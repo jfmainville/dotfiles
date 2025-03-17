dc55c226578b6a04d29a3aa64d8f878e3dc7ccce
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-03-16 21:01:19 -0400

    refactor: re-add the fugitive plugin to the project

---
+++ lua/plugins/vim-fugitive.lua
@@ -0,0 +1,32 @@
+return {
+	"tpope/vim-fugitive",
+	lazy = false,
+	keys = {
+		-- {
+		-- 	mode = "n",
+		-- 	"<leader>gg",
+		-- 	"<cmd>tab Git<CR>",
+		-- 	desc = "Open the vim-fugitive git interface",
+		-- },
+		{
+			mode = "n",
+			"<leader>gl",
+			"<cmd>vertical Git log -p -- %<CR>",
+			desc = "Open the vim-fugitive git log interface for the current file in the buffer",
+		},
+		{
+			mode = "n",
+			"<leader>gL",
+			":tab Git log --graph<CR>",
+			desc = "Open the vim-fugitive git log interface for the current file in the buffer",
+		},
+		{
+			mode = "n",
+			"<leader>gp",
+			"<cmd>Git push<CR>",
+			desc = "Execute the git push command through vim-fugitive",
+		},
+		{ mode = "n", "<leader>gh", ':Git commit -m "', desc = "Quick commit using vim-fugitive" },
+		{ mode = "n", "<leader>ga", ':Git commit -am "', desc = "Quick commit all using vim-fugitive" },
+	},
+}
