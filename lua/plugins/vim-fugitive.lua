ceed99a80692eab66e871b4a481c2962414eb44b
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-03-16 21:31:38 -0400

    refactor: update the keymaps configuration for the fugitive plugin

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,24 +1,22 @@
 return {
 	"tpope/vim-fugitive",
+	dependencies = {
+		"tpope/vim-rhubarb", -- GitHub Git browser
+		"cedarbaum/fugitive-azure-devops.vim", -- Azure DevOps Git browser
+	},
 	lazy = false,
 	keys = {
-		-- {
-		-- 	mode = "n",
-		-- 	"<leader>gg",
-		-- 	"<cmd>tab Git<CR>",
-		-- 	desc = "Open the vim-fugitive git interface",
-		-- },
 		{
 			mode = "n",
 			"<leader>gl",
-			"<cmd>vertical Git log -p -- %<CR>",
+			"<cmd>horizontal Git log -p -- %<CR>",
 			desc = "Open the vim-fugitive git log interface for the current file in the buffer",
 		},
 		{
 			mode = "n",
 			"<leader>gL",
-			":tab Git log --graph<CR>",
-			desc = "Open the vim-fugitive git log interface for the current file in the buffer",
+			"<cmd>horizontal Git log<CR>",
+			desc = "Open the vim-fugitive git log interface for all commits",
 		},
 		{
 			mode = "n",
@@ -26,7 +24,39 @@ return {
 			"<cmd>Git push<CR>",
 			desc = "Execute the git push command through vim-fugitive",
 		},
-		{ mode = "n", "<leader>gh", ':Git commit -m "', desc = "Quick commit using vim-fugitive" },
-		{ mode = "n", "<leader>ga", ':Git commit -am "', desc = "Quick commit all using vim-fugitive" },
+		{
+			mode = "n",
+			"<leader>gP",
+			"<cmd>Git pull --rebase --autostash<CR>",
+			desc = "Execute the git push command through vim-fugitive",
+		},
+		{
+			mode = "n",
+			"<leader>gh",
+			function()
+				vim.cmd("Gitsigns stage_hunk")
+				vim.cmd("Gitsigns preview_hunk_inline")
+				vim.fn.feedkeys(':Git commit -m "')
+			end,
+			"",
+			desc = "Quick commit the current hunk using vim-fugitive",
+		},
+		{
+			mode = "n",
+			"<leader>ga",
+			function()
+				vim.cmd("Git add %")
+				vim.fn.feedkeys(':Git commit -m "')
+			end,
+			desc = "Quick commit all the hunks in the current file using vim-fugitive",
+		},
 	},
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "fugitiveblame", "fugitive" },
+			callback = function()
+				vim.api.nvim_buf_set_keymap(0, "n", "<Tab>", "=", { silent = true })
+			end,
+		})
+	end,
 }
