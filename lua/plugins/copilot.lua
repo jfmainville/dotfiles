443a36d919488e2ea2edd1eb186ca0f8c7c28983
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-03-29 22:46:58 -0400

    refactor: update the copilot plugin base configuration to include suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,13 +2,30 @@ return {
 	"zbirenbaum/copilot.lua",
 	lazy = false,
 	event = "InsertEnter",
+	keys = {
+		{
+			mode = "n",
+			"<leader>tc",
+			"<cmd>Copilot suggestion<CR>",
+			desc = "Toggle the Copilot suggestions",
+		},
+	},
 	config = function()
 		require("copilot").setup({
 			panel = {
 				enabled = false,
 			},
 			suggestion = {
-				enabled = false,
+				enabled = true,
+				auto_trigger = true,
+				keymap = {
+					accept = "<C-CR>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-j>",
+					prev = "<C-k>",
+					dismiss = "<C-e>",
+				},
 			},
 			filetypes = {
 				yaml = true,
