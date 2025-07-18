5dafe857e4b94318fdfbd25e21d5a5f2a4e641ba
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-07-17 22:15:09 -0400

    fix(smear-cursor): add options to prevent cursor drawing over target in command mode

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,4 +1,11 @@
 return {
 	"sphamba/smear-cursor.nvim",
-	opts = {},
+	opts = {
+		enabled = true,
+		smear_terminal_mode = false,
+
+		-- This is a fix to prevent the cursor from being drawn over the target primarily an issue in command mode
+		hide_target_hack = true,
+		never_draw_over_target = true,
+	},
 }
