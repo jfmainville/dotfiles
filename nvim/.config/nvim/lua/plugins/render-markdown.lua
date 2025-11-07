c90ace2c8695155d0fc2109eb0ff9a0fc44dc244
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-11-06 22:16:53 -0500

    feat(plugins): add render-markdown.nvim with keybinding for markdown preview, update lockfile

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		keys = {
+			{
+				mode = "n",
+				"<leader>tm",
+				"<cmd>RenderMarkdown toggle<CR>",
+				desc = "Toggle the Markview markdown preview mode",
+			},
+		},
+		opts = {},
+	},
+}
