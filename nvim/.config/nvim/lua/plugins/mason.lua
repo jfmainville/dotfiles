a00418e0025dd7fe5c24dcb2500a7d236cacc303
Jean-Frederic Mainville <jfmainville@outlook.com>
2026-01-21 17:02:05 -0500

    feat(lsp)!: refactor the LSP configuration with the new approach

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -3,142 +3,110 @@ return {
 	dependencies = { "neovim/nvim-lspconfig", "williamboman/mason-lspconfig.nvim" },
 	config = function()
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-		local lsp_servers = {
-			ts_ls = {},
-			eslint = {},
-			cssls = {
-				capabilities = capabilities,
-			},
-			cssmodules_ls = {},
-			tailwindcss = {},
-			emmet_language_server = {},
-			ansiblels = {
-				settings = {
+		vim.lsp.enable("ts_ls")
+		vim.lsp.enable("eslint")
+		vim.lsp.config("cssls", {
+			capabilities = capabilities,
+		})
+		vim.lsp.enable("cssls")
+		vim.lsp.enable("cssmodules_ls")
+		vim.lsp.enable("tailwindcss")
+		vim.lsp.enable("emmet_language_server")
+		vim.lsp.config("ansiblels", {
+			settings = {
+				ansible = {
 					ansible = {
-						ansible = {
-							path = "ansible",
-						},
-						executionEnvironment = {
-							enabled = false,
-						},
-						python = {
-							interpreterPath = "python",
-						},
-						validation = {
+						path = "ansible",
+					},
+					executionEnvironment = {
+						enabled = false,
+					},
+					python = {
+						interpreterPath = "python",
+					},
+					validation = {
+						enabled = true,
+						lint = {
 							enabled = true,
-							lint = {
-								enabled = true,
-								path = "ansible-lint",
-							},
+							path = "ansible-lint",
 						},
 					},
 				},
 			},
-			dockerls = {},
-			yamlls = {
-				settings = {
-					yaml = {
-						format = {
-							enable = true,
+		})
+		vim.lsp.enable("ansiblels")
+		vim.lsp.enable("dockerls")
+		vim.lsp.config("yamlls", {
+			settings = {
+				yaml = {
+					format = {
+						enable = true,
+					},
+					schemas = {
+						["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.1-standalone-strict/all.json"] = "*/kubernetes/*.yaml",
+						["http://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
+						["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+							"*/pipeline/*.yml",
+							"*/.pipeline/*.yml",
+							"*/pipeline/*.yaml",
+							"*/.pipeline/*.yaml",
+							"*/.build/*.yml",
+							"*/.build/*.yaml",
 						},
-						schemas = {
-							["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.1-standalone-strict/all.json"] = "*/kubernetes/*.yaml",
-							["http://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
-							["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
-								"*/pipeline/*.yml",
-								"*/.pipeline/*.yml",
-								"*/pipeline/*.yaml",
-								"*/.pipeline/*.yaml",
-								"*/.build/*.yml",
-								"*/.build/*.yaml",
-							},
-						},
-						schemastore = {
-							enable = true,
-						},
+					},
+					schemastore = {
+						enable = true,
 					},
 				},
 			},
-			pyright = {},
-			bashls = {},
-			lua_ls = {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
+		})
+		vim.lsp.enable("yamlls")
+		vim.lsp.enable("pyright")
+		vim.lsp.enable("bashls")
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
 					},
 				},
 			},
-			jsonls = {},
-			prismals = {},
-			intelephense = {},
-			rust_analyzer = {},
-			terraformls = {},
-			tflint = {},
-			typos_lsp = {},
-			marksman = {},
-		}
-
-		-- Need to customized the installed LSPs based on the operating system and architecture used
-		local function is_debian_arm64()
-			local f = io.popen("dpkg --print-architecture")
-			local arch = f:read("*a")
-			f:close()
-
-			return arch:match("arm64") ~= nil
-		end
-
-		local function is_wsl()
-			local wsl_env = os.getenv("WSLENV")
-			return wsl_env ~= nil
-		end
-
-		if is_debian_arm64() then
-			-- Create a function to remove an item from a table using a string
-			local function remove_item(tbl, item)
-				for i, v in pairs(tbl) do
-					if v == item then
-						return table.remove(tbl, i)
-					end
-				end
-			end
-
-			remove_item(lsp_servers, "lua_ls")
-		end
-
-		if is_wsl() then
-			-- Create a function to remove an item from a table using a string
-			local function add_item(tbl, item)
-				for i, v in pairs(tbl) do
-					if v == item then
-						return table.insert(tbl, i)
-					end
-				end
-			end
-
-			add_item(lsp_servers, "powershell_es")
-			vim.lsp.config("powershell_es", {
-				filetypes = { "ps1", "psm1", "psd1" },
-				bundle_path = "C:/Users/jfmainville/Documents/Tools/PowerShellEditorServices",
-				shell = "powershell.exe",
-				settings = { powershell = { codeFormatting = { Preset = "OTBS" } } },
-				init_options = {
-					enableProfileLoading = false,
-				},
-			})
-		end
+		})
+		vim.lsp.enable("lua_ls")
+		vim.lsp.enable("jsonls")
+		vim.lsp.enable("prismals")
+		vim.lsp.enable("intelephense")
+		vim.lsp.enable("rust_analyzer")
+		vim.lsp.enable("terraformls")
+		vim.lsp.enable("tflint")
+		vim.lsp.enable("typos_lsp")
+		vim.lsp.enable("marksman")
 
 		require("mason").setup({})
 		require("mason-lspconfig").setup({
-			ensure_installed = lsp_servers,
-			handlers = {
-				function(server_name)
-					local server = lsp_servers[server_name] or {}
-					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
-				end,
+			ensure_installed = {
+				"ts_ls",
+				"eslint",
+				"cssls",
+				"cssmodules_ls",
+				"tailwindcss",
+				"emmet_language_server",
+				"ansiblels",
+				"dockerls",
+				"yamlls",
+				"pyright",
+				"bashls",
+				"lua_ls",
+				"jsonls",
+				"prismals",
+				"intelephense",
+				"rust_analyzer",
+				"terraformls",
+				"tflint",
+				"typos_lsp",
+				"marksman",
 			},
 		})
 
