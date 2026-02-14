92c0da4b6fad3be999f63c111f0b2187e2d49c6d
Jean-Frederic Mainville <jfmainville@outlook.com>
2026-02-13 23:12:18 -0500

    chore(noice): remove the lsp custom configuration

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -16,17 +16,6 @@ return {
 				},
 			},
 		},
-		lsp = {
-			signature = {
-				enabled = false, -- Need to disable the signature help to prevent overlapping with the nvim-cmp plugin
-			},
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-			},
-		},
 		-- you can enable a preset for easier configuration
 		presets = {
 			bottom_search = true, -- use a classic bottom cmdline for search
