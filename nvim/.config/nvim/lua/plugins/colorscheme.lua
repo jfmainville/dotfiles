1c12a256da48ec45dbb190466b93302f87dfa1c8
Jean-Frederic <jfmainville@outlook.com>
2026-05-02 22:45:52 -0400

    refactor(colorscheme): update the colorscheme configuration

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,8 @@
 return {
-	"folke/tokyonight.nvim",
+	dir = "~/projects/fluovibe.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "moon",
-			transparent = true, -- Enable transparent background
-			terminal_colors = true, -- Enable terminal colors
-			styles = {
-				sidebars = "transparent", -- Transparent sidebars
-				floats = "transparent", -- Transparent floating windows
-			},
-		})
-		vim.cmd("colorscheme tokyonight-moon")
+		vim.cmd("colorscheme fluovibe")
 	end,
 }
