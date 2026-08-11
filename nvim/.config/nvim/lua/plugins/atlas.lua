a1ed9bedacdd6f137b17e50d4830d31aaa453571
Jean-Frederic Mainville <jfmainville@outlook.com>
2026-08-11 14:44:32 -0400

    feat(atlas): add additional configurations

--- nvim/.config/nvim/lua/plugins/atlas.lua
+++ nvim/.config/nvim/lua/plugins/atlas.lua
@@ -6,15 +6,38 @@ return {
 		"MeanderingProgrammer/render-markdown.nvim",
 		"esmuellert/codediff.nvim",
 	},
+	keys = {
+		{
+			mode = "n",
+			"<leader>op",
+			"<cmd>AtlasPulls<cr>",
+			desc = "Open the list of PRs",
+		},
+	},
 	opts = {
 		pulls = {
 			providers = {
-				github = {}, -- See configuration below
+				github = {
+					views = {
+						{
+							name = "Me",
+							key = "1",
+							layout = "plain",
+							search = "author:@me assignee:@me sort:updated-desc",
+						},
+						{
+							name = "Team",
+							key = "2",
+							layout = "plain",
+							search = "assignee:@me assignee:mbonin_gswc assignee:fbgirard_gswc assignee:pbtrudel_gswc assignee:pojasmin_gswc org:sherweb-operations org:sherweb-development sort:updated-desc",
+						},
+					},
+				},
 			},
 		},
 		issues = {
 			providers = {
-				github = {}, -- See configuration below
+				github = {},
 			},
 		},
 	},
