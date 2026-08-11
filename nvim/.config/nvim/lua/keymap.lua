8d728a9b8dfa1eea2ccac7328d32410fa5087f81
Jean-Frederic Mainville <jfmainville@outlook.com>
2026-08-11 14:44:15 -0400

    fix(keymap): update the search alignment keymaps

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -47,5 +47,15 @@ vim.keymap.set("n", "<TAB>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<S-TAB>", "<cmd>cprev<CR>")
 vim.keymap.set("n", "<leader>co", "<cmd>copen<CR>")
 -- Center view when moving to the next (n) or previous (N) search result
-vim.keymap.set("n", "n", "nzzzv", { desc = "Center search next" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Center search prev" })
+vim.keymap.set("n", "n", function()
+	if vim.fn.mode() == "n" and vim.v.searchforward ~= nil then
+		return "nzzzv"
+	end
+	return "n"
+end, { expr = true, desc = "Center search next" })
+vim.keymap.set("n", "N", function()
+	if vim.fn.mode() == "n" and vim.v.searchforward ~= nil then
+		return "Nzzzv"
+	end
+	return "N"
+end, { expr = true, desc = "Center search previous" })
